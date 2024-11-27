f01af57dc8b62e57a57cdd7f5ddaaed4fd7c8a47
Roma <darkfoulz_btw@list.ru>
2024-11-27 18:42:44 +0500

    Измененна перенменная

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import math
 #Функция Алгоритма Расчет
-def Distance(x0, y0, x1, y1):
-    distance = math.sqrt((x1 - x0)**2 + (y0 - y1)**2)
-    return print(f"Вычисленное Расстояние между двумя точками на плоскости ({x0}, {y0}) и ({x1}, {y0}): {distance}")
+def Distance(x, y, x1, y1):
+    distance = math.sqrt((x1 - x)**2 + (y - y1)**2)
+    return print(f"Вычисленное Расстояние между двумя точками на плоскости ({x}, {y}) и ({x1}, {y1}): {distance}")
 
 
 if __name__ == "__main__":
-    x0 = float(input("Введите точку координаты x0:"))
-    y0 = float(input("Введите точку координаты y0:"))
+    x = float(input("Введите точку координаты x0:"))
+    y = float(input("Введите точку координаты y0:"))
     x1 = float(input("Введите точку координаты x1:"))
     y1 = float(input("Введите точку координаты y1:"))
     Distance(x_0, y_0, x_1, y_1)
