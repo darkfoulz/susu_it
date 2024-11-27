e7e0f265e401ed08ec5b714ffdc4016fad176c8e
Roma <darkfoulz_btw@list.ru>
2024-11-27 17:40:46 +0500

    Рассчетная функция

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-# Функция Алгоритма Расчета
+#Алгоритма Расчета
 def Distance(x_0, y_0, x_1, y_1):
     distance = math.sqrt((x_1 - x_0)**2 + (y_0 - y_1)**2)
     return print(f"Расстояние между двумя точками ({x_0}, {y_0}) и ({x_1}, {y_0}): {distance}")
