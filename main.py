c4c92b973cc2a696c8c68db54ffccbeec49ad67c
Roma <darkfoulz_btw@list.ru>
2024-11-27 15:19:50 +0500

    Программа для расчета длины отрезка

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+
+def Distance(x_0, y_0, x_1, y_1):
+    distance = math.sqrt((x_1 - x_0)**2 + (y_0 - y_1)**2)
+    return print(f"Длина расстояние между двух точек {distance}")
+
+if __name__ == "__main__":
+    x_0 = int(input("Введите точку координаты x0:"))
+    y_0 = int(input("Введите точку координаты y0:"))
+    x_1 = int(input("Введите точку координаты x1:"))
+    y_1 = int(input("Введите точку координаты y1:"))
+    Distance(x_0, y_0, x_1, y_1)
